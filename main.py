f90eeaace82d4b4a51a7db8b6454f49bb773add2
Rūta <135346302+ruta-is-coding@users.noreply.github.com>
2024-09-07 10:32:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-#This is a text encryption-decryption program Caesar Cipher
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
+def caesar(original_text, shift_amount, encode_or_decode):
+    final_text = ""
+    if encode_or_decode == 'decode':
+        shift_amount *= -1
+
     for letter in original_text:
+        if letter not in alphabet:
+            final_text += letter
+            continue
+
         original_index = alphabet.index(letter)
         new_index = original_index + shift_amount
         new_index %= len(alphabet)
+        final_text += alphabet[new_index]
 
-        encrypted_text += alphabet[new_index]
-    print(f"Here is the encoded result: {encrypted_text}")
+    print(f"Here is the {encode_or_decode}d result: {final_text}")
 
 
-encrypt(original_text=text, shift_amount=shift)
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+
+    if direction != 'encode' and direction != 'decode':
+        print("\nWrong choice! Please type 'encode' or 'decode'.\n")
+        continue
+
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+
+    if restart == 'yes':
+        continue
+    elif restart == 'no':
+        break
+    else:
+        print("Wrong input choice!")
+        break
